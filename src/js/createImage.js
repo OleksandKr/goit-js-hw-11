b8ecd7a3e1fc3c079ebd169d3252d159721e8db8
OleksandKr <cfif357@ukr.net>
2023-05-22 18:04:19 +0300

    task-11

---
+++ src/js/createImage.js
@@ -0,0 +1,25 @@
+﻿export function createImage(images) {
+    return images
+      .map(
+        image => `<div class="photo-card">
+    <a href="${image.largeImageURL}">
+  <img src="${image.webformatURL}" alt="${image.tags}" class="gallery-image" loading="lazy" />
+  </a>
+  <div class="info">
+    <p class="info-item">${image.likes}
+      <b>Likes</b>
+    </p>
+    <p class="info-item">${image.views}
+      <b>Views</b>
+    </p>
+    <p class="info-item">${image.comments}
+      <b>Comments</b>
+    </p>
+    <p class="info-item">${image.downloads}
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`
+      )
+      .join('');
+};
---
+++ src/js/fetchImages.js
@@ -0,0 +1,25 @@
+﻿import axios from 'axios';
+
+export const DEFAULT_PAGE = 1;
+export let page = DEFAULT_PAGE;
+    
+export const perPage = 40;
+const KEY = '36641889-67bfddc05989bf2e3819f9b8e';
+
+export async function fetchImages(searchValue) {
+    const searchParams = new URLSearchParams({
+      key: KEY,
+      q: searchValue,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: perPage,
+      page,
+    });
+    const images = await axios.get(`https://pixabay.com/api/?${searchParams}`).then(page +=1);
+    return images.data;
+};
+
+export function resetPage() {
+    page = DEFAULT_PAGE;
+};
